9de8c6597bd4e09d57ececfc5a517d12d9ba5aad
wilgnerschuertz <wilgnerdeveloper@gmail.com>
2025-06-09 07:32:05 -0400

    fix: comando no backend e add cors.

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+
+// Configurando CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());
